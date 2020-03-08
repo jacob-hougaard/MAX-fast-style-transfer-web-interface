042b3d9da7d8d10d271efff972193e5f70d213e4
PadawanMiyagi <jacob.bennedsen@ibm.com>
2020-03-08 13:34:34 +0100

    Added build folder

---
+++ build/precache-manifest.88e4634e89007cc544886220ce866db6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16ca4247516be59556dd82d2d7c7ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aaa21d41b9192b2b0c5",
+    "url": "/static/css/main.7aebfd28.chunk.css"
+  },
+  {
+    "revision": "76302d0d9e39b1b9bdb8",
+    "url": "/static/js/2.5b8000ef.chunk.js"
+  },
+  {
+    "revision": "8d1a37553594a39f585a245d66470763",
+    "url": "/static/js/2.5b8000ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aaa21d41b9192b2b0c5",
+    "url": "/static/js/main.27ab8cd3.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
